522fc4a4b171a8e651cdfe3f3d5d9ff7f444a747
namhee <namhee0812@gmail.com>
2022-11-18 09:44:33 +0900

    feat: Add StringAddCalculator class and StringAddCalculatorTest class
    Add TestCases for StringAddCalculator First for TDD ways

--- src/main/java/Calculator/StringAddCalculator.java
+++ src/main/java/Calculator/StringAddCalculator.java
@@ -1,9 +1,30 @@
 package Calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String text) {
-        return 0;
+        if (text == null || text == "") {
+            return 0;
+        }
+        Integer result = stringSplitValidator(text);
+        if(result == 0) {
+            result = Integer.parseInt(text);
+        }
+        return result;
     }
 
+    public static Integer stringSplitValidator(String text)
+    {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+            return 0;
+        }
+        String[] tokens= text.split(",|:");
+        return 0;
+    }
 }
